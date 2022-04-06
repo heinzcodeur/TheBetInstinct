812957d719b787ed1cb7711da41ea997a56c6b18
heinzcodeur <heinzcodeur@gmail.com>
2022-04-06 22:17:16 +0200

    testenv

--- src/Entity/Pays.php
+++ src/Entity/Pays.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\PaysRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -26,6 +28,16 @@ class Pays
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $shortcut;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Villes::class, mappedBy="pays")
+     */
+    private $villes;
+
+    public function __construct()
+    {
+        $this->villes = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -52,6 +64,36 @@ class Pays
     public function setShortcut(?string $shortcut): self
     {
         $this->shortcut = $shortcut;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Villes>
+     */
+    public function getVilles(): Collection
+    {
+        return $this->villes;
+    }
+
+    public function addVille(Villes $ville): self
+    {
+        if (!$this->villes->contains($ville)) {
+            $this->villes[] = $ville;
+            $ville->setPays($this);
+        }
+
+        return $this;
+    }
+
+    public function removeVille(Villes $ville): self
+    {
+        if ($this->villes->removeElement($ville)) {
+            // set the owning side to null (unless already changed)
+            if ($ville->getPays() === $this) {
+                $ville->setPays(null);
+            }
+        }
 
         return $this;
     }
---
+++ src/Entity/Villes.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VillesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=VillesRepository::class)
+ */
+class Villes
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Pays::class, inversedBy="villes")
+     */
+    private $pays;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPays(): ?Pays
+    {
+        return $this->pays;
+    }
+
+    public function setPays(?Pays $pays): self
+    {
+        $this->pays = $pays;
+
+        return $this;
+    }
+}
